a1f2c57d2d1003bbf54c0a18c4cf5d1702247e7a
Humbertzhang <504490160@qq.com>
2017-04-03 23:08:32 +0800

    fix something

---
+++ rmUseless.sh
@@ -0,0 +1,8 @@
+echo 'romove    celerybeat.pid'
+echo '          celerybeat-schedule'
+echo '          make_celery.pyc'
+echo '          monitor.pyc'
+rm celerybeat.pid
+rm celerybeat-schedule
+rm make_celery.pyc
+rm monitor.pyc
--- stop.sh
+++ stop.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 sudo killall -9 redis-server
 killall -9 celery
+echo  'The flask has not been killed'
+echo  'Please stop it by its PID if you want'
