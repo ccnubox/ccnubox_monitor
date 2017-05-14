3bb3a9fa1a5270ab35d97540157b9e25ba3dba9b
Humbertzhang <504490160@qq.com>
2017-05-14 15:13:59 +0800

    announcement-404 delete_class-404 edit_class-401 inquire_classes_table-401inquire_grades-502

--- start.sh
+++ start.sh
@@ -3,6 +3,5 @@ sudo nginx &
 sudo redis-server /etc/redis/redis.conf &
 celery worker --app monitor.celery &
 celery beat --app monitor.celery &
-uwsgi --ini uwsgi.ini &
-python monitor.py &
-echo 'running on 127.0.0.1:5001'
+uwsgi --ini app.ini &
+echo 'running on 127.0.0.1:6000'
---
+++ wsgi.py
@@ -0,0 +1,5 @@
+from monitor import app
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host = '119.29.147.14',port = 6000)
+
